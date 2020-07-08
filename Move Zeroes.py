125c584974fbfe6d922b417dfeae45d544035491
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-08 10:01:23 +1000

    Add files via upload

---
+++ Move Zeroes.py
@@ -0,0 +1,15 @@
+#Move Zeroes
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for num in nums:
+            if num != 0:
+                nums[i] = num
+                i += 1
+        for j in xrange(i, len(nums)):
+            nums[j] = 0
