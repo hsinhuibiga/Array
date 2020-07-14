f93e14e6e589b1138b25a54e70d5ecbd6d7eb270
hsinhuibiga <hsinhuibiga@gmail.com>
2020-07-14 10:00:29 +1000

    Add files via upload

---
+++ Two Sum.py
@@ -0,0 +1,16 @@
+#Two Sum
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        container = {}
+
+        for i, num in enumerate(nums):
+            if target - num in container:
+                return [container[target - num], i]
+            container[num] = i
+        return
